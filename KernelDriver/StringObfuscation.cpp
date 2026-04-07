f27f38eb85476872088acfa31b576d28936483f4
NikolisSecurity <nikolis.balis@gmail.com>
2026-04-08 00:38:51 +0300

    Remove KernelDriver and UmbrellaCore folders

--- KernelDriver/StringObfuscation.cpp
+++
@@ -1,9 +0,0 @@
-// KernelDriver/StringObfuscation.cpp
-#include "StringObfuscation.h"
-
-// Simple XOR-based decryption
-VOID DecryptString(_Inout_ PCHAR EncryptedString, _In_ ULONG Length, _In_ UCHAR Key) {
-    for (ULONG i = 0; i < Length; ++i) {
-        EncryptedString[i] ^= Key;
-    }
-}
--- KernelDriver/build_now.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\Nikolis\Documents\Umbrella Spoofer\UmbrellaSpoofer\KernelDriver"
-"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\MSBuild.exe" UmbrellaKernelDriver.vcxproj /p:Configuration=Release /p:Platform=x64 /p:SpectreMitigation=false /p:SpectreMitigationCheck=false /p:SignMode=Off /v:minimal
-echo EXITCODE=%ERRORLEVEL%
